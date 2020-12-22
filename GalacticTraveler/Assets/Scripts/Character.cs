4f21f439a28f99eb0443e27cace4629d37b5f7d5
JesusMachy <65319410+JesusMachy@users.noreply.github.com>
2020-12-22 20:19:02 +0100

    Player Script Added
    and Window startup

---
+++ GalacticTraveler/Assets/Scripts/Character.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Character
+{
+    // Vitals
+    public string Name;
+    public int Level;
+
+    // Base Attributes
+    public int BaseStrength;
+    public int BaseAgility;
+    public int BasePerception;
+    public int BaseToughness;
+    public int BaseInteligence;
+    public int BaseCharisma;
+
+
+}
--- GalacticTraveler/Assets/Scripts/GameManager.cs
+++ GalacticTraveler/Assets/Scripts/GameManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    public Character Player;
+
     // Start is called before the first frame update
     void Start()
     {
